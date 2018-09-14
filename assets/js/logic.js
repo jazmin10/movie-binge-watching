76d1bafed9f5776ceb7c4cca2027baa8375e10c7
jazmin10 <jazminestrada10@gmail.com>
2018-09-14 18:32:17 -0500

    displaying top three search results in the modal window

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,9 +7,11 @@ $(document).ready(() => {
 		
 		let userInput = $("#movie-input").val().trim();
 
+		// If a blank form was entered, then mark input form "red"
 		if (userInput === "") {
 			$("#movie-input").addClass("form-error");
 		}
+		// Otherwise, if a value was entered, then start the search
 		else {
 			$("#movie-input").val("");
 
@@ -29,32 +31,68 @@ $(document).ready(() => {
 			url: `http://www.omdbapi.com/?apikey=4b1d9a31&type=movie&r=json&page=1&s=${movieName}`,
 			method: "GET"
 		}).then(response => {
+			// If we obtain results, then display the movie results
 			displayResults(response);
 		}).catch(err => {
+			// If an error occurred, then alert user
 			alert("Oops! Something went wrong");
 		});
 	};
 
 	// Displays top three results of the search
 	let displayResults = movieResults => {
+		$("#results-modal .modal-body").empty();
+
+		// If results are returned, then display top three results in the modal
 		if (movieResults.Response === "True") {
 			let movieCount = 3;
+			let movies = movieResults.Search;
 
-			movieResults.Search.forEach(currMovie => {
-				
-			})
-		} 
+			let row = $("<div>");
+			row.addClass("row");
+
+			// For the top three results, display movie title, poster, and add button
+			for (var i = 0; i < movieCount; i++) {
+				let movieDiv = $("<div>");
+				let titleDiv = $("<h5>");
+				let imgTag = $("<img>");
+				let button = $("<button>");
+
+				movieDiv.addClass("col-md-4 movie-result");
+				button.addClass("btn btn-primary");
+
+				imgTag.attr("src", movies[i].Poster);
+				button.attr("type", "submit");
+
+				titleDiv.text(movies[i].Title);
+				button.text("Add");
+
+				movieDiv.append(titleDiv).append(imgTag).append(button);
+
+				row.append(movieDiv);
+			}
+
+			$("#results-modal .modal-body").append(row);
+		}
+		// If a response was returned, but that response has no search results... 
 		else {
 			$("#results-modal .modal-body").html("<h4>No results. Try again.</h4>");
-
-			$("#results-modal").modal("toggle");
 		}
+
+		// Show the modal
+		$("#results-modal").modal("toggle");
 	};
 
 
 // ====================== MAIN PROCESSES ======================
 
 	$("#search-form .btn").on("click", movieSearch);
+
+	// $("#search-form .btn").on("click", event => {
+	// 	event.preventDefault();
+
+	// 	$("#results-modal").modal("toggle");
+	// })
 
 	$("#movie-input").focus(formFocus);
 });

5d52a788ce47d2551725af658d76582cb2b29aae
jazmin10 <jazminestrada10@gmail.com>
2018-09-17 17:41:07 -0500

    completed api routing

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -8,45 +8,100 @@
 
 // ====================== FUNCTIONS ======================
 	let allMovies = response => {
-		console.log("display all movies");
+
+		// Response with an array of the movies in the db
+		Movie.findAll().then(results => {
+			response.json(results);
+		});
 	};
 
 	let addMovie = (request, response) => {
-		console.log("add movie");
+		// Store title, year, and img values from the body of the request
+		let {title, year, img} = request.body;
+
+		let movie = {
+			title: title,
+			year: year,
+			img: img
+		};
+
+		// Add movie to the db and respond with the new row values
+		Movie.create(movie).then(results => {
+			response.json(results);
+		});
 	};
 
 	let updateView = (request, response) => {
-		console.log("update viewed");
+		// Store viewed value from the body of the request
+		let { viewed } = request.body;
+		// Store id of the movie from the request url
+		let { id } = request.params;
+
+		let newValue = { viewed: viewed };
+		let condition = { where: { id: id } };
+
+		// Update the viewed value of the specified movie
+		// and respond with number of affected rows
+		Movie.update(newValue, condition).then(results => {
+			response.json(results);
+		});
 	};
 
 	let updateComments = (request, response) => {
-		console.log("update comments");
+		// Store the comments value from the body of the request
+		let { comments } = request.body;
+		// Store the id of the movie from the request url
+		let { id } = request.params;
+
+		let newValue = { comments: comments };
+		let condition = { where: { id: id }};
+
+		// Update the comments value of the specified movie
+		// and response with the number of affected rows
+		Movie.update(newValue, condition).then(results => {
+			response.json(results);
+		});
 	};
 
 	let removeMovie = (request, response) => {
-		console.log("remove movie");
+		// Store the id value from the request url
+		let { id } = request.params;
+
+		let condition = { where: { id: id }};
+
+		// Delete the specified movie and return the number of
+		// affected rows
+		Movie.destroy(condition).then(results => {
+			response.json(results);
+		});
 	};
 
 // ====================== MAIN PROCESSES ======================
 
 	module.exports = app => {
+
+		// Finds all movies in the db
 		app.get("/api/all", (allReq, allResp) => {
 			allMovies(allResp);
 		});
 
+		// Adds a movie to the db
 		app.post("/api/new", (newReq, newResp) => {
 			addMovie(newReq, newResp);
 		});
 
+		// Updates the viewed value of a specific movie in the db
 		app.put("/api/view/:id", (updtReq, updtResp) => {
 			updateView(updtReq, updtResp);
 		});
 
+		// Updates comments of a specific movie in the db
 		app.put("/api/comments/:id", (updtReq, updtResp) => {
 			updateComments(updtReq, updtResp);
 		});
 
-		app.delete("/api/remove/:id", (rmvReq, rmvResp) => {
+		// Deletes a movie from the db
+		app.delete("/api/delete/:id", (rmvReq, rmvResp) => {
 			removeMovie(rmvReq, rmvResp);
 		});
 	};
